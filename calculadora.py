c2dec1f63fdf2748bd913c57dad42d4ffb70280e
arlenesr28 <arlene.sr28@gmail.com>
2016-09-21 12:02:10 -0600

    arreglando

--- calculadora.py
+++ calculadora.py
@@ -1,15 +1,16 @@
 def Menu():
     """Funcion que Muestra el Menu"""
-    print """************
-Calculadora
-************
-Menu
-------------
-1) Suma
-2) Resta
-3) Multiplicacion
-4) Division
-5) Salir"""
+    print """   ************
+                Calculadora
+                ************
+                Menu
+                ------------
+                1) Suma
+                2) Resta
+                3) Multiplicacion
+                4) Division
+                5) Salir
+          """
 
 def Calculadora():
     """Funcion Para Calcular Operaciones Aritmeticas"""
@@ -34,3 +35,6 @@ def Calculadora():
             except ZeroDivisionError:
               print "No se Permite la Division Entre 0"
               opc = int(input("Selecione Opcion\n"))
+
+
+Calculadora ()
